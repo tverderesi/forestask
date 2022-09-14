8c87b3a3d3158df24a86e18d843e56efe757067f
Thomas Verderesi <tverderesi@gmail.com>
2022-09-14 18:30:35 -0300

    Started refactoring Florestarefa app to React.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular, brands, icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { BsCardChecklist, BsFillMoonStarsFill, BsFillPersonFill } from "react-icons/bs";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Helmet from "react-helmet";
+import "./App.css";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Helmet>
+        <title>Florestarefa 🌲</title>
+      </Helmet>
+      <header className="text-center mt-3">
+        <h1 className="fw-bolder">
+          flores<span className="forest-green">tarefa </span>
+          <FontAwesomeIcon icon={solid("tree")} />
+        </h1>
+      </header>
+      <Nav
+        id="main_nav"
+        class="navbar fixed-bottom navbar-light bg-light fa-lg row
+    justify-self-center"
+      >
+        <label class="theme-switch col-1 flex-column" for="checkbox">
+          <div>
+            <input type="checkbox" id="checkbox" class="btn" title="dark mode" />
+            <BsFillMoonStarsFill />
+          </div>
+        </label>
+        <button
+          class="btn flex-column col-1 text-center font-weight-bold"
+          data-toggle="collapse"
+          href="#information-card"
+          aria-pressed="false"
+          autocomplete="off"
+        >
+          <div>
+            <BsFillPersonFill />
+          </div>
+          Perfil <br />
+        </button>
+        <button
+          id="show-things"
+          type="button"
+          class="btn flex-column col-1 text-center
+        font-weight-bold"
+          data-toggle="collapse"
+          href="#cards-container"
+          aria-pressed="false"
+          autocomplete="off"
+        >
+          <div>
+            <BsCardChecklist />
+          </div>
+          Mostrar
+          <br /> Tarefas
+        </button>
+
+        <button
+          href="./html/teachermode.html"
+          className="btn flex-column font-weight-bold text-center"
+        >
+          <div>
+            <FaChalkboardTeacher />
+            <i class="bi bi-person-workspace fa-lg"></i>
+          </div>
+          <div>
+            Modo
+            <br />
+            Professor
+          </div>
+        </button>
+      </Nav>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +88,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
