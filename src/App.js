f9e6d43c52f54824c0ec4321f59652c3abe50958
Thomas Verderesi <tverderesi@gmail.com>
2022-09-15 03:37:28 -0300

    Moved Navbar into its own component.

--- src/App.js
+++ src/App.js
@@ -1,94 +1,74 @@
-import ReactDOM from "react-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid, regular, brands, icon } from "@fortawesome/fontawesome-svg-core/import.macro";
-import { BsCardChecklist, BsFillMoonStarsFill, BsFillPersonFill } from "react-icons/bs";
+import { BsCardChecklist, BsFillMoonStarsFill, BsFillPersonFill, BsTreeFill } from "react-icons/bs";
 import { FaChalkboardTeacher } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Helmet from "react-helmet";
 import "./App.css";
 import Nav from "react-bootstrap/Nav";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <Helmet>
-        <title>Florestarefa 🌲</title>
-      </Helmet>
-      <header className="text-center mt-3">
-        <h1 className="fw-bolder">
-          flores<span className="forest-green">tarefa </span>
-          <FontAwesomeIcon icon={solid("tree")} />
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url("./media/4.png")`,
+        height: "100vh",
+        margin: "0",
+        overflow: "hidden",
+        backgroundSize: "cover",
+        backgroundPosition: "0 100vh",
+      }}
+    >
+      <header className="mt-3">
+        <h1 className=" d-flex align-items-center justify-content-center fw-bold">
+          flores<span className="forest-green me-3">tarefa </span>
+          <BsTreeFill className="forest-green" />
         </h1>
       </header>
-      <Nav
-        id="main_nav"
-        class="navbar fixed-bottom navbar-light bg-light fa-lg row
-    justify-self-center"
-      >
-        <label class="theme-switch col-1 flex-column" for="checkbox">
-          <div>
-            <input type="checkbox" id="checkbox" class="btn" title="dark mode" />
-            <BsFillMoonStarsFill />
-          </div>
-        </label>
-        <button
-          class="btn flex-column col-1 text-center font-weight-bold"
-          data-toggle="collapse"
-          href="#information-card"
-          aria-pressed="false"
-          autocomplete="off"
-        >
-          <div>
-            <BsFillPersonFill />
-          </div>
-          Perfil <br />
-        </button>
-        <button
-          id="show-things"
-          type="button"
-          class="btn flex-column col-1 text-center
-        font-weight-bold"
-          data-toggle="collapse"
-          href="#cards-container"
-          aria-pressed="false"
-          autocomplete="off"
-        >
-          <div>
-            <BsCardChecklist />
-          </div>
-          Mostrar
-          <br /> Tarefas
-        </button>
 
-        <button
-          href="./html/teachermode.html"
-          className="btn flex-column font-weight-bold text-center"
-        >
-          <div>
-            <FaChalkboardTeacher />
-            <i class="bi bi-person-workspace fa-lg"></i>
-          </div>
-          <div>
-            Modo
-            <br />
+      {/* <Nav >
+        <Nav.Item>
+          <Button variant="transparent">
+            <div className="mb-2">
+              <BsFillMoonStarsFill size="1.3em" />
+            </div>
+            Noite
+          </Button>{" "}
+        </Nav.Item>
+        <Nav.Item>
+          {" "}
+          <Button variant="transparent" data-toggle="collapse" href="#information-card">
+            <div className="mb-2">
+              <BsFillPersonFill size="1.5em" />
+            </div>
+            Perfil
+          </Button>
+        </Nav.Item>
+
+        <Nav.Item>
+          <Button
+            id="show-things"
+            type="button"
+            variant="transparent"
+            data-toggle="collapse"
+            href="#cards-container"
+            aria-pressed="false"
+            autocomplete="off"
+          >
+            <div className="mb-2">
+              <BsCardChecklist size="1.5em" />
+            </div>
+            Tarefas
+          </Button>
+        </Nav.Item>
+        <Nav.Item>
+          <Button href="./html/teachermode.html" variant="transparent">
+            <div className="mb-2">
+              <FaChalkboardTeacher size="1.5em" />
+            </div>
             Professor
-          </div>
-        </button>
-      </Nav>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+          </Button>
+        </Nav.Item>
+      </Nav>*/}
     </div>
   );
 }
