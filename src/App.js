9f89e68364f68c6b22991cc6e27e96b7cdb1daa4
Thomas Verderesi <tverderesi@gmail.com>
2022-10-05 16:36:42 -0300

    added 'cardlist' component.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import React from "react";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import CardList from './components/CardList';
 
 function App() {
   const style = {
     backgroundImage: `url("./media/4.png")`,
-    height: "90vh",
-    margin: "0",
-    overflow: "hidden",
-    backgroundSize: "cover",
-    backgroundPosition: "0 90%",
-  }
+    height: '90vh',
+    margin: '0',
+    overflow: 'hidden',
+    backgroundSize: 'cover',
+    backgroundPosition: '0 90%',
+  };
   return (
-<>
-<div
-      className="App"
-      style={style}>
-      <Header />
-    </div>
-    <Navbar/></>
+    <>
+      <div
+        className='App'
+        style={style}
+      >
+        <Header />
+        <CardList />
+      </div>
+
+      <Navbar />
+    </>
   );
 }
 
