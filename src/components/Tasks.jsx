886f142e63a3192a40ccb157be1143e19c4589b9
Thomas Verderesi <tverderesi@gmail.com>
2022-10-15 21:38:49 -0300

    Cleaned up button code.

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,25 +1,24 @@
-import { BsCardChecklist } from "react-icons/bs";
-import { Button } from "react-bootstrap";
-import React from "react";
+import { BsCardChecklist } from 'react-icons/bs';
+import { Button } from 'react-bootstrap';
+import React from 'react';
+import { FaTree } from 'react-icons/fa';
 
 function Tasks() {
+  return (
+    <Button
+      id='show-things'
+      type='button'
+      variant='transparent'
+      data-toggle='collapse'
+      aria-pressed='false'
+      autocomplete='off'
+    >
+      <div className='mb-2'>
+        <FaTree size='1.5em' />
+      </div>
+      Forest
+    </Button>
+  );
+}
 
-    return (
-      <Button
-        id="show-things"
-        type="button"
-        variant="transparent"
-        data-toggle="collapse"
-        href="#cards-container"
-        aria-pressed="false"
-        autocomplete="off"
-      >
-        <div className="mb-2">
-          <BsCardChecklist size="1.5em" />
-        </div>
-        Tarefas
-      </Button>
-    );
-  }
-
-  export default Tasks
+export default Tasks;
