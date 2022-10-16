61958591f904614658b9ed958924397cd78d0c2c
Thomas Verderesi <tverderesi@gmail.com>
2022-10-15 21:37:45 -0300

    Redesigned header to fit inside Profile card.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 
 function Header() {
   return (
-    <header
-      className='mt-3'
-      style={{ background: 'none' }}
-    >
-      <h1 className='d-flex align-items-center justify-content-center fw-bold'>
-        flores<span className='forest-green me-3'>tarefa </span>
-        <BsTreeFill className='forest-green' />
-      </h1>
+    <header style={{ background: 'none' }}>
+      <div className='d-flex mt-1'>
+        <div
+          style={{ width: 'auto' }}
+          className='mx-auto fw-bold h3 w-auto align-self-center'
+        >
+          flores<span className='forest-green'>tarefa </span>
+        </div>
+        <div style={{ position: 'absolute', left: '90%' }}>
+          <BsTreeFill className='forest-green h3 justify-self-end' />
+        </div>
+      </div>
     </header>
   );
 }
