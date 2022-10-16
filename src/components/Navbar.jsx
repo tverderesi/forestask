026cd213ae285eda1e35e997c2ab0995a6bd91a7
Thomas Verderesi <tverderesi@gmail.com>
2022-10-15 21:38:04 -0300

    Cleaned up Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,10 @@
-import { Nav } from 'react-bootstrap';
-
 function Navbar({ navItems }) {
   return (
-    <Nav
-      justify
-      className='fixed-bottom bg-light align-items-center p-2 space-around'
-      style={{ height: '12vh', backdropFilter: 'blur(20px)' }}
-    >
-      {navItems}
-    </Nav>
+    <div className='d-flex justify-content-around fw-bold'>
+      {navItems.map(navItem => {
+        return <>{navItem}</>;
+      })}
+    </div>
   );
 }
 
