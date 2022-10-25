632df66d9dcfa0a20cf9cff9a2d62a048c0f40c7
Thomas Verderesi <tverderesi@gmail.com>
2022-10-25 00:05:22 -0300

    Removed PalleteCreator from type.

--- src/types/ContextTypes.ts
+++ src/types/ContextTypes.ts
@@ -1,11 +1,9 @@
 import {ReactNode} from 'react'
-
 export interface CardListContextTypes {
     cards: any;
     page: number;
     pageLimit: number;
     numCards: any;
-    palleteCreator: any;
     handleClick: any;
   }
   
