75c19354561de9bd18db08f66b964a00bead7f08
Thomas Verderesi <tverderesi@gmail.com>
2022-10-25 00:00:34 -0300

    moved component

---
+++ src/components/buttons/TeacherMode.tsx
@@ -0,0 +1,12 @@
+import { FaChalkboardTeacher } from 'react-icons/fa';
+
+export default function TeacherMode() {
+  return (
+    <button className='btn btn-transparent'>
+      <div className='mb-2'>
+        <FaChalkboardTeacher size='1.5em' />
+      </div>
+      Professor
+    </button>
+  );
+}
