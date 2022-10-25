4725d5afdda578378d8bd5c395d438e0f29a9146
Thomas Verderesi <tverderesi@gmail.com>
2022-10-24 23:59:10 -0300

    moved component.

---
+++ src/components/interfaceGeneral/navItems.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import DarkMode from '../buttons/DarkMode';
+import Tasks from '../buttons/Tasks';
+import TeacherMode from '../buttons/TeacherMode';
+
+export const navItems = [<DarkMode />, <Tasks />, <TeacherMode />];
--- src/components/navItems.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import DarkMode from './DarkMode';
-import Tasks from './Tasks';
-import TeacherMode from './TeacherMode';
-
-export const navItems = [<DarkMode />, <Tasks />, <TeacherMode />];
