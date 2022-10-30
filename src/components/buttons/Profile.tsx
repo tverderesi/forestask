9b0edfd064238078086c5303355f6b24a8ab68b5
Thomas Verderesi <tverderesi@gmail.com>
2022-10-30 17:00:37 -0300

    Removed BS button element.

--- src/components/buttons/Profile.tsx
+++ src/components/buttons/Profile.tsx
@@ -1,18 +1,16 @@
 import { BsFillPersonFill } from 'react-icons/bs';
-import { Button } from 'react-bootstrap';
 
 function Profile() {
   return (
-    <Button
-      variant='transparent'
+    <button
+      className='btn btn-transparent'
       data-toggle='collapse'
-      href='#information-card'
     >
       <div className='mb-2'>
         <BsFillPersonFill size='1.5em' />
       </div>
       Perfil
-    </Button>
+    </button>
   );
 }
 
