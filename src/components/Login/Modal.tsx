18159480da68774bf83b324bef19294e008c8f2c
Thomas Verderesi <tverderesi@gmail.com>
2022-10-31 16:43:09 -0300

    Added modal component for handling login

---
+++ src/components/Login/Modal.tsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react';
+import CardListContext from '../../context/CardListContext';
+import Loading from './Loading';
+import Login from './Login';
+
+export default function Modal() {
+  const { loginSuccess } = useContext(CardListContext);
+  return (
+    <div
+      style={{
+        zIndex: '10',
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        width: '100vw',
+        height: '100vh',
+        backdropFilter: 'blur(50px)',
+        backgroundColor: '#dfdfdfb3',
+      }}
+      className='d-flex flex-column align-self-center justify-self-center align-items-center justify-content-center'
+    >
+      {loginSuccess ? <Loading /> : <Login />}
+    </div>
+  );
+}
