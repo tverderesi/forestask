6b6b6a5518115226bcc2608cd2c21f98fffc1780
Thomas Verderesi <tverderesi@gmail.com>
2022-11-03 02:31:40 -0300

    Added the rest of the cases.

--- src/context/AppReducer.tsx
+++ src/context/AppReducer.tsx
@@ -1,22 +1,56 @@
-interface action {
-  type: string;
-  payload: any;
-}
+import palleteCreator from '../components/style/PaletteCreator';
 
-type state = any;
-
-export default function AppReducer(action: action, state: state) {
+export const AppReducer = (state: any, action: any) => {
+  const subjectTextBaseColor = 'hsl(276, 53%, 51%)';
   switch (action.type) {
     default:
       return state;
     case 'LOGIN_SUCCESS':
       return { ...state, loginSuccess: true };
     case 'LOAD_TOTAL_CARDS':
-      return { ...state, cards: action.payload, loadingCards: true };
-    case 'TOTAL_CARDS_SUCCESS':
-      return { ...state, totalCards: action.payload };
-
-    case 'LOAD_CARDS_SUCCESS':
-      return { ...state, loadingCards: false, totalCards: action.payload };
+      return { ...state, totalCards: action.payload, loading: true };
+    case 'RENDER_FIRST_CARD':
+      return { ...state, cards: action.payload, loading: true };
+    case 'LOAD_SUBJECTS':
+      const subjectPallete = palleteCreator(
+        action.payload,
+        subjectTextBaseColor
+      );
+      return {
+        ...state,
+        subjects: action.payload,
+        subjectPallete: subjectPallete,
+        loading: true,
+      };
+    case 'SET_PAGES_PARAMETERS':
+      return {
+        ...state,
+        cardsPerPage: action.payload.cardsPerPage,
+        maxPages: action.payload.maxPages,
+        loading: true,
+      };
+    case 'CLEAR_CARDS':
+      return { ...state, cards: [], loading: true };
+    case 'RENDER_CARDS':
+      return { ...state, cards: action.payload, loading: true };
+    case 'LOAD_SUCCESS':
+      return { ...state, loadSuccess: true };
+    case 'CHANGE_PAGE':
+      return {
+        ...state,
+        page: action.payload.newPage,
+        cards: action.payload.currentCards,
+      };
+    case 'FETCH_ACTIVITIES':
+      return { ...state, activities: action.payload };
+    case 'CREATE_PALETTES':
+      return {
+        ...state,
+        subjectPalette: action.payload.subjectPalette,
+        activityPalette: action.payload.activityPalette,
+      };
+    case 'ADD_TO_FILTER':
+      console.log(action.payload);
+      return { ...state, filters: action.payload };
   }
-}
+};
