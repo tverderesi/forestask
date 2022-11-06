15fbaa6839c3b3eace7ccdc6a86f6c633f86b6c5
Thomas Verderesi <tverderesi@gmail.com>
2022-11-06 13:49:50 -0300

    New Filters Added.

---
+++ src/components/profile/AccordionCompleted.tsx
@@ -0,0 +1,42 @@
+import { Container } from 'react-bootstrap';
+
+require('react-datepicker/dist/react-datepicker-cssmodules.css');
+
+export default function AccordionCompleted({ filters, dispatch }) {
+  return (
+    <Container
+      fluid
+      className='d-flex align-items-center justify-content-around g-0 justify-self-center'
+    >
+      <label>
+        <input
+          type='radio'
+          name='Completed'
+          id='completed'
+          value='checked'
+          className='me-1'
+          onClick={() => {
+            const payload = { ...filters, checked: true };
+            dispatch({ type: 'ADD_TO_FILTER', payload: payload });
+          }}
+        />
+        Completed
+      </label>
+
+      <label>
+        <input
+          type='radio'
+          name='Completed'
+          id='completed'
+          value='not-checked'
+          className='me-1'
+          onClick={() => {
+            const payload = { ...filters, checked: false };
+            dispatch({ type: 'ADD_TO_FILTER', payload: payload });
+          }}
+        />
+        Not Completed
+      </label>
+    </Container>
+  );
+}
--- src/components/profile/AccordionDeadline.tsx
+++ src/components/profile/AccordionDeadline.tsx
@@ -1,27 +1,56 @@
+import { useContext, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import AppContext from '../../context/AppContext';
+
+require('react-datepicker/dist/react-datepicker-cssmodules.css');
 
 export default function AccordionDeadline() {
+  const { dispatch, filters } = useContext(AppContext);
+  const [focused, setFocused] = useState(false);
   return (
     <Container
       fluid
       className='d-flex align-items-center justify-content-around g-0 justify-self-center'
     >
-      <input
-        type='date'
+      <div
+        className='d-flex flex-column jusitfy-content-center align-items-center'
         style={{
           borderRadius: '10rem',
-          border: 'none',
+
           fontWeight: '600',
           height: '2.25rem',
-          paddingLeft: '7%',
+
           paddingRight: '0px',
           width: '70%',
-          paddingBottom: '2%',
+          outline: focused ? '3px solid #c491ff' : '',
           backgroundColor: '#e9ecef',
           fontSize: '.85rem',
+          alignSelf: 'center',
         }}
-        className=' text-center'
-      />
+      >
+        <input
+          type='date'
+          className=' text-center'
+          onChange={(e: any) => {
+            const payload = { ...filters, deadline: e.target.value };
+            dispatch({ type: 'ADD_TO_FILTER', payload: payload });
+          }}
+          style={{
+            width: '80%',
+            marginTop: 'auto',
+            marginBottom: 'auto',
+            backgroundColor: 'transparent',
+            border: 'none',
+            fontWeight: '600',
+          }}
+          onFocus={() => {
+            setFocused(true);
+          }}
+          onBlur={() => {
+            setFocused(false);
+          }}
+        />
+      </div>
     </Container>
   );
 }
