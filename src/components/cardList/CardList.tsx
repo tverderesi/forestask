378954936db4fd82256febe8b9d9588d9827717a
Thomas Verderesi <tverderesi@gmail.com>
2022-11-06 13:49:04 -0300

    Added card when filter returns 'none'.

--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -3,6 +3,9 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
 import AppContext from '../../context/AppContext';
 import CardListTop from './CardListTop';
+import { Card } from 'react-bootstrap';
+import { MdSentimentVerySatisfied } from 'react-icons/md';
+import { GiPartyPopper } from 'react-icons/gi';
 
 function CardList() {
   const { maxPages, cardsPerPage, windowWidth, cards, page } =
@@ -41,33 +44,86 @@ function CardList() {
           <CardListTop />
         </motion.div>
 
-        {cards.map((item, index) => {
-          return (
-            <motion.div
-              key={item.id}
-              initial={{ opacity: 0, x: -200 }}
-              animate={{
-                x: 0,
-                opacity: 1,
-                transition: {
-                  duration: 0.3,
-                  delay: 0.45 + 0.15 * (index % cardsPerPage),
-                },
+        {cards.length ? (
+          cards.map((item, index) => {
+            return (
+              <motion.div
+                key={item.id}
+                initial={{ opacity: 0, x: -200 }}
+                animate={{
+                  x: 0,
+                  opacity: 1,
+                  transition: {
+                    duration: 0.3,
+                    delay: 0.45 + 0.15 * (index % cardsPerPage),
+                  },
+                }}
+                exit={{
+                  opacity: 0,
+                  x: 200,
+                  transition: {
+                    duration: 0.3,
+                    delay: 0.15 * (index % cardsPerPage),
+                  },
+                }}
+                className={page === maxPages ? `mb-3` : ''}
+              >
+                <ActivityCard item={item} />
+              </motion.div>
+            );
+          })
+        ) : (
+          <motion.div
+            key='nocards'
+            initial={{ opacity: 0, x: -200 }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              transition: {
+                duration: 0.3,
+                delay: 0.15,
+              },
+            }}
+            exit={{
+              opacity: 0,
+              x: 200,
+              transition: {
+                duration: 0.3,
+                delay: 0.15,
+              },
+            }}
+          >
+            <Card
+              className='d-flex justify-content-center'
+              style={{
+                backdropFilter: 'blur(20px)',
+                backgroundColor: '#f9fafb80',
+                borderRadius: '16px',
+                border: 'none',
+                boxShadow: '5px 5px 20px #3a3a3a38',
+                height: '12.5vh',
               }}
-              exit={{
-                opacity: 0,
-                x: 200,
-                transition: {
-                  duration: 0.3,
-                  delay: 0.15 * (index % cardsPerPage),
-                },
-              }}
-              className={page === maxPages ? `mb-3` : ''}
             >
-              <ActivityCard item={item} />
-            </motion.div>
-          );
-        })}
+              <Card.Body
+                className='d-flex justify-content-center align-items-center mb-0'
+                style={{ fontSize: '1.25em', fontWeight: '600' }}
+              >
+                <span className='h5 mb-0'>
+                  No Activities found for these filters. Hooray!
+                </span>
+                <GiPartyPopper
+                  className='ms-3'
+                  size={70}
+                  style={{
+                    position: 'absolute',
+                    right: '2%',
+                    color: '#a6a6a6b3',
+                  }}
+                />
+              </Card.Body>
+            </Card>
+          </motion.div>
+        )}
       </AnimatePresence>
     </div>
   );
