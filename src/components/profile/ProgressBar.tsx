56451ed8e5d9df33c7ce4bc6f209417791ab92a2
Thomas Verderesi <tverderesi@gmail.com>
2022-11-07 20:39:11 -0300

    Progress Bar now shows correct XP% for level.

---
+++ src/components/profile/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import { setLastLevel } from '../../context/AppFunctions';
+
+export default function ProgressBar({ userData, gameLevels }) {
+  const lastLevel = setLastLevel(gameLevels);
+  const nextLevel = (Number(userData.level) + 1).toString();
+  const progressWidth = (userData, gameLevels) => {
+    if (userData.level === lastLevel) {
+      return '100%';
+    } else {
+      const levelPercentage =
+        ((userData.xp - gameLevels[userData.level]) /
+          (gameLevels[nextLevel] - gameLevels[userData.level])) *
+        100;
+      return `${levelPercentage}%`;
+    }
+  };
+  return (
+    <div
+      className='progress-bar'
+      style={{
+        backgroundColor: '#c491ff',
+        width: progressWidth(userData, gameLevels),
+      }}
+    ></div>
+  );
+}
