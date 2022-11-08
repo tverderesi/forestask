0109e28a0ff98d674c760ae5a8ebd4b3e3236f5d
Thomas Verderesi <tverderesi@gmail.com>
2022-11-08 01:13:10 -0300

    tweaked card style.

--- src/components/activityCard/CardTags.tsx
+++ src/components/activityCard/CardTags.tsx
@@ -12,42 +12,53 @@ import { IconContext } from 'react-icons/lib';
 function CardTags({ subject, type, deadline, xp }: Types.CardTags) {
   const { subjectPalette, activityPalette } = useContext(AppContext);
   // Styling
+  const locale = Intl.NumberFormat().resolvedOptions().locale;
   const classes =
-    'd-flex align-items-center col-auto me-3 g-0 justify-self-center';
-  const date = new Date(deadline).toLocaleDateString();
+    'd-flex align-items-center me-1 col-auto g-0 justify-self-center';
+
+  const date = new Date(deadline).toLocaleDateString(locale, {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
 
   return (
     <IconContext.Provider value={{ size: '20px' }}>
       <div
-        className='d-flex justify-content-between flex-wrap g-0 w-100'
-        style={{ fontWeight: 600 }}
+        className='d-flex flex-wrap g-0 w-100'
+        style={{ fontWeight: 600, fontSize: '.9rem' }}
       >
         <div
           className={classes}
-          style={{ width: '22%' }}
+          style={{ width: '6.75rem' }}
         >
           <MdCalendarToday className='me-1' />
           {date}
         </div>
         <div
           className={classes}
-          style={{ width: '15%' }}
+          style={{ width: '4.5rem' }}
         >
           <MdSentimentVerySatisfied className='me-1' />
           {Number(xp)} XP
         </div>
         <div
-          className={classes}
-          style={{ color: activityPalette[`${type}`], width: '22%' }}
+          className='d-flex g-0 mt-1'
+          style={{ width: '13.5rem' }}
         >
-          <MdInfoOutline className='me-1' /> {type}
-        </div>
-        <div
-          className={classes}
-          style={{ color: subjectPalette[`${subject}`], width: '22%' }}
-        >
-          <MdOutlineLightbulb className='me-1' />
-          {subject}
+          <div
+            className={classes}
+            style={{ color: activityPalette[`${type}`], width: '50%' }}
+          >
+            <MdInfoOutline className='me-1' /> {type}
+          </div>
+          <div
+            className={classes}
+            style={{ color: subjectPalette[`${subject}`], width: '50%' }}
+          >
+            <MdOutlineLightbulb className='me-1' />
+            {subject}
+          </div>
         </div>
       </div>
     </IconContext.Provider>
