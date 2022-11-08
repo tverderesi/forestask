ec5eb0946b093cab6f2526504d899028c840bb17
Thomas Verderesi <tverderesi@gmail.com>
2022-11-08 01:15:52 -0300

    Tweaked INIT states to streamline first loading.

--- src/context/AppReducer.tsx
+++ src/context/AppReducer.tsx
@@ -5,12 +5,22 @@ export const AppReducer = (state: any, action: any) => {
   switch (action.type) {
     default:
       return state;
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload };
     case 'LOGIN_SUCCESS':
-      return { ...state, loginSuccess: true };
+      return { ...state, loginSuccess: 'LOGIN_SUCCESS' };
     case 'LOAD_TOTAL_CARDS':
-      return { ...state, totalCards: action.payload, loading: true };
+      return {
+        ...state,
+        totalCards: action.payload,
+        loadingStatus: 'LOGIN_SUCCESS',
+      };
     case 'RENDER_FIRST_CARD':
-      return { ...state, cards: action.payload, loading: true };
+      return {
+        ...state,
+        cards: action.payload,
+        loadingStatus: 'RENDER_FIRST_CARD',
+      };
     case 'LOAD_SUBJECTS':
       const subjectPallete = palleteCreator(
         action.payload,
@@ -21,20 +31,25 @@ export const AppReducer = (state: any, action: any) => {
         subjects: action.payload,
         subjectPallete: subjectPallete,
         loading: true,
+        loadingStatus: 'LOAD_SUBJECTS',
       };
     case 'SET_PAGES_PARAMETERS':
       return {
         ...state,
         cardsPerPage: action.payload.cardsPerPage,
         maxPages: action.payload.maxPages,
-        loading: true,
+        loadingStatus: 'SET_PAGES_PARAMETERS',
       };
     case 'CLEAR_CARDS':
-      return { ...state, cards: [], loading: true };
+      return { ...state, cards: [], loadingStatus: 'CLEAR_CARDS' };
     case 'RENDER_CARDS':
-      return { ...state, cards: action.payload, loading: true };
-    case 'LOAD_SUCCESS':
-      return { ...state, loadSuccess: true };
+      return { ...state, cards: action.payload, loadingStatus: 'RENDER_CARDS' };
+    case 'INIT_SUCCESS':
+      return {
+        ...state,
+        initSuccess: true,
+        loadingStatus: 'INIT_SUCCESS',
+      };
     case 'CHANGE_PAGE':
       return {
         ...state,
@@ -42,21 +57,46 @@ export const AppReducer = (state: any, action: any) => {
         cards: action.payload.currentCards,
       };
     case 'FETCH_ACTIVITIES':
-      return { ...state, activities: action.payload };
+      return {
+        ...state,
+        activities: action.payload,
+        loadingStatus: 'FETCH_ACTIVITIES',
+      };
     case 'CREATE_PALETTES':
       return {
         ...state,
         subjectPalette: action.payload.subjectPalette,
         activityPalette: action.payload.activityPalette,
+        loadingStatus: 'CREATE_PALETTES',
       };
     case 'ADD_TO_FILTER':
       return { ...state, filters: action.payload };
 
     case 'SET_GAME_LEVELS':
-      return { ...state, gameLevels: action.payload, loading: true };
+      return {
+        ...state,
+        gameLevels: action.payload,
+        loadingStatus: 'SET_GAME_LEVELS',
+      };
     case 'SET_PAGE':
       return { ...state, page: action.payload };
     case 'SET_USER_DATA':
-      return { ...state, userData: action.payload, loading: true };
+      return {
+        ...state,
+        userData: action.payload,
+        loadingStatus: 'SET_USER_DATA',
+      };
+    case 'UPDATE_PAGES_PARAMETERS':
+      return {
+        ...state,
+        cardsPerPage: action.payload.cardsPerPage,
+        maxPages: action.payload.maxPages,
+      };
+
+    case 'UPDATE_PAGE':
+      return {
+        ...state,
+        cards: action.payload,
+      };
   }
 };
