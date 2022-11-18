0215f8ae974dc4eeadd5e454376b2eba041b2588
Thomas Verderesi <tverderesi@gmail.com>
2022-11-18 09:58:01 -0300

    moved style to main css file.

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,8 @@ import { AppProvider } from './context/AppContext';
 import Viewport from './components/Viewport';
 
 function App() {
-  const style = {
-    backgroundImage: `url("./media/background/4.png")`,
-    height: '100vh',
-    margin: '0',
-    overflow: 'hidden',
-    backgroundSize: 'cover',
-    backgroundPosition: '0 100%',
-  };
-
   return (
-    <div
-      className='App d-flex justify-content-center'
-      style={style}
-    >
+    <div className='App d-flex justify-content-center'>
       <AppProvider>
         <Viewport />
       </AppProvider>
