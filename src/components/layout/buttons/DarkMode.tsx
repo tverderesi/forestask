c521ca3330c5e3c6d4514c5c5edab4a92c140d53
Thomas Verderesi <tverderesi@gmail.com>
2022-11-18 10:05:24 -0300

    Added dark mode handling.

--- src/components/layout/buttons/DarkMode.tsx
+++ src/components/layout/buttons/DarkMode.tsx
@@ -1,15 +1,24 @@
+import { useContext } from 'react';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
+import AppContext from '../../../context/AppContext';
 
 function DarkMode() {
+  const { dataTheme, dispatch } = useContext(AppContext);
   return (
     <button
       className='btn btn-transparent'
       key='darkMode'
+      onClick={() => {
+        dispatch({
+          type: 'CHANGE_COLOR',
+          payload: dataTheme === 'dark' ? 'light' : 'dark',
+        });
+      }}
     >
       <div className='mb-2'>
         <BsFillMoonStarsFill size='1.3em' />
       </div>
-      Noite
+      {dataTheme === 'dark' ? 'Day' : 'Night'}
     </button>
   );
 }
