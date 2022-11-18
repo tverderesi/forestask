4540d3045986751cd2083b80db53162883a21ee3
Thomas Verderesi <tverderesi@gmail.com>
2022-11-18 10:05:10 -0300

    Added forest component.

--- src/components/forest/ForestBackground.tsx
+++ src/components/forest/ForestBackground.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import Tree from './Tree';
+import { motion } from 'framer-motion';
 
-export default function ForestBackground({ levelArray = [0] }) {
+export default function ForestBackground({
+  levelArray = [0],
+  windowWidth,
+  showForest,
+}) {
+  const classDesktop =
+    'd-flex flex-row justify-content-around align-items-center';
   return (
-    <div
-      className='d-flex flex-row justify-content-around align-items-end'
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      key='Forest'
+      className={
+        windowWidth < 825 ? 'd-flex align-items-center ' : classDesktop
+      }
       style={{
         position: 'absolute',
-        top: '65vh',
-
+        bottom: '0',
+        overflow: 'hidden',
         zIndex: '0',
-        width: '100vw',
-        height: 'fit-content',
+        width: windowWidth < 825 ? '100vw' : '100vw',
+        height: '50vh',
       }}
     >
       {levelArray.map(i => {
         return <Tree number={i + 1} />;
       })}
-    </div>
+    </motion.div>
   );
 }
--- src/components/forest/Tree.tsx
+++ src/components/forest/Tree.tsx
@@ -1,20 +1,24 @@
+import { motion } from 'framer-motion';
 export default function Tree({ number }) {
   const positionTop = (number % 4) * (number % 2 ? -1 : 1);
 
   return (
-    <div>
-      <img
+    <motion.div>
+      <motion.img
         src={`./media/trees/${number % 5}.png`}
+        key={`tree ${number}`}
         alt={`tree ${number}`}
         style={{
           position: 'relative',
           top: `${positionTop}vh`,
           filter: 'brightness(1.3) hue-rotate(-15deg)',
           scale: '2',
-
           zIndex: '0',
         }}
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 100 }}
       />
-    </div>
+    </motion.div>
   );
 }
