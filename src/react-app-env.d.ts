6eba8e8d1a321f3dba61c139d4bf6aece6723759
Thomas Verderesi <tverderesi@gmail.com>
2022-11-18 09:58:15 -0300

    Added environment variables.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,7 +3,9 @@
 declare namespace NodeJS {
     interface ProcessEnv {
        //types of envs
-        NODE_ENV:'development'| 'production' | 'test';
+        NODE_ENV: 'development' | 'production' | 'test';
+        REACT_APP_PUBLIC_URL: string;
+        REACT_APP_FAKE_SERVER: string;
 
 
     }
