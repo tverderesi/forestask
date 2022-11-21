28fe8884e9345994602870a0fe8d7b86b92efead
Thomas Verderesi <tverderesi@gmail.com>
2022-11-21 01:26:50 -0300

    style changes.

--- src/components/layout/buttons/DarkMode.tsx
+++ src/components/layout/buttons/DarkMode.tsx
@@ -1,25 +1,32 @@
 import { useContext } from 'react';
+import { Col } from 'react-bootstrap';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
 import AppContext from '../../../context/AppContext';
 
 function DarkMode() {
   const { dataTheme, dispatch } = useContext(AppContext);
   return (
-    <button
-      className='btn btn-transparent'
-      key='darkMode'
-      onClick={() => {
-        dispatch({
-          type: 'CHANGE_COLOR',
-          payload: dataTheme === 'dark' ? 'light' : 'dark',
-        });
-      }}
-    >
-      <div className='mb-2'>
-        <BsFillMoonStarsFill size='1.3em' />
-      </div>
-      {dataTheme === 'dark' ? 'Day' : 'Night'}
-    </button>
+    <Col xs={4}>
+      <button
+        className='btn btn-transparent d-flex flex-column justify-content-between w-100'
+        key='darkMode'
+        onClick={() => {
+          dispatch({
+            type: 'CHANGE_COLOR',
+            payload: dataTheme === 'dark' ? 'light' : 'dark',
+          });
+        }}
+        style={{ height: '10vh' }}
+      >
+        <div className='mb-2'>
+          <BsFillMoonStarsFill
+            size={20}
+            style={{ verticalAlign: 'top' }}
+          />
+        </div>
+        {dataTheme === 'dark' ? 'Day' : 'Night'}
+      </button>
+    </Col>
   );
 }
 
