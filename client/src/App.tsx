020c1f67d87b9666c5450ae61c81b6db304aaa20
Thomas Verderesi <tverderesi@gmail.com>
2023-01-31 02:33:33 -0300

    Added Apollo Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,46 @@
 import './App.css';
 import { AppProvider } from './context/AppContext';
-import Viewport from './components/Viewport';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import HomeButtons from './components/layout/login/HomeButtons';
+import Modal from './components/login/Modal';
+import Register from './components/pages/Register';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 function App() {
+  const client = new ApolloClient({
+    //@ts-ignore
+
+    cache: new InMemoryCache(),
+  });
   return (
-    <div className='App d-flex justify-content-center'>
-      <AppProvider>
-        <Viewport />
-      </AppProvider>
-    </div>
+    <main className='App flex justify-center bg-image'>
+      <ApolloProvider client={client}>
+        <AppProvider>
+          <Router>
+            <Routes>
+              <Route
+                element={<Home />}
+                path='/'
+              >
+                <Route
+                  path='/'
+                  element={<HomeButtons />}
+                />
+                <Route
+                  path='/login'
+                  element={<Modal />}
+                />
+                <Route
+                  path='/register'
+                  element={<Register />}
+                />
+              </Route>
+            </Routes>
+          </Router>
+        </AppProvider>
+      </ApolloProvider>
+    </main>
   );
 }
 
