a44317f9f7d6eae504dab0a3215ecbe7834ca9b8
tverderesi <tverderesi@gmail.com>
2023-04-29 02:17:51 -0300

    updated hero

--- client/src/pages/Hero.tsx
+++ client/src/pages/Hero.tsx
@@ -13,7 +13,7 @@ export const Hero = ({ heroText = "" }) => {
       <h1 className="text-5xl md:text-5xl lg:text-7xl  text-white mb-10 text-center h-full relative flex items-center mx-5 leading-relaxed md:leading-relaxed lg:leading-relaxed transition-all ">
         <p>{heroText}</p>
       </h1>
-      <div className="flex flex-col md:flex-row w-1/2 md:w-auto justify-center space-x-0 space-y-5 md:space-x-5 md:space-y-0 absolute bottom-[12.5%] md:bottom-1/4">
+      <div className="flex flex-col md:flex-row w-1/2 md:w-auto justify-center space-x-0 space-y-5 md:space-x-5 md:space-y-0 relative bottom-[12.5%] md:bottom-1/4">
         <button className="bg-night-900 text-seal-brown-50 hover:bg-mantis hover:text-night-900 btn btn-lg md:btn-2xl border-none drop-shadow-lg">
           Learn More
         </button>
