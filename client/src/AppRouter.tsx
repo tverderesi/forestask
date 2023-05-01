aa27ac314d39d01b59bffd4213753a4565f21bff
tverderesi <tverderesi@gmail.com>
2023-05-01 16:03:52 -0300

    moved server into folder

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -9,16 +9,18 @@ import AppHome from "./pages/app/home/AppHome";
 
 import AdminHome from "./pages/app/home/AdminHome";
 import ProfileEditPage from "./pages/ProfileEditPage";
+import Loading from "./components/layout/Loading";
 export function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="*" element={<NotFound />} />
-        <Route
-          path="/"
-          element={<PageLayout />}
-          errorElement={<NotFound />}
-        ></Route>
+        <Route path="/" element={<PageLayout />} errorElement={<NotFound />}>
+          <Route
+            path="/loading"
+            element={<Loading className="w-screen h-screen" text={"loading"} />}
+          />
+        </Route>
         <Route path="/admin" element={<AdminHome />} />
 
         <Route path="/app/" element={<Onboard />} errorElement={<NotFound />}>
