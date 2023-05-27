0a006af99060eac64d2293e78ba4e53ae44cb697
tverderesi <tverderesi@gmail.com>
2023-05-27 01:46:56 -0300

    refactor: page waits to font to load

--- client/src/atoms/interface/Navbar.tsx
+++ client/src/atoms/interface/Navbar.tsx
@@ -1,50 +1,67 @@
 import { Link } from "react-router-dom";
 import Logo from "../Logo";
 import { useScrollToSection } from "../../util/hooks";
+import { useContext } from "react";
+import AppContext from "../../context/AppContext";
+import { SingleTreeLoader } from "./SingleTreeLoader";
+
 export default function Navbar({ children }) {
   const scrollToHero = useScrollToSection("#hero");
+  const { fontLoaded } = useContext(AppContext);
   return (
     <header className="navbar bg-night-900 text-lavender-web-100 h-16 fixed z-10">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label
-            tabIndex={0}
-            className="btn btn-ghost btn-dark text-lavender-web lg:hidden "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+      {!fontLoaded ? (
+        <SingleTreeLoader
+          treeNumber={0}
+          className="stroke-white h-full mx-auto"
+        />
+      ) : (
+        <>
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label
+                tabIndex={0}
+                className="btn btn-ghost btn-dark text-lavender-web lg:hidden "
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu dropdown-content mt-3 p-2 shadow bg-night-900 rounded-box w-[calc(95vw)] font-semibold text-center  "
+              >
+                {children}
+              </ul>
+            </div>
+            <button onClick={scrollToHero}>
+              <Logo light={false} className="ml-0" />
+            </button>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{children}</ul>
+          </div>
+          <div className="navbar-end">
+            <Link
+              to="/app"
+              className="font-bold btn btn-ghost bg-seal-brown-600 hover:bg-seal-brown-500 active:bg-seal-brown-500"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu dropdown-content mt-3 p-2 shadow bg-night-900 rounded-box w-[calc(95vw)] font-semibold text-center  "
-          >
-            {children}
-          </ul>
-        </div>
-        <button onClick={scrollToHero}>
-          <Logo light={false} className="ml-0" />
-        </button>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{children}</ul>
-      </div>
-      <div className="navbar-end">
-        <Link to="/app" className="btn bg-seal-brown-400">
-          App
-        </Link>
-      </div>
+              App
+            </Link>
+          </div>
+        </>
+      )}
     </header>
   );
 }
--- client/src/organisms/Hero.tsx
+++ client/src/organisms/Hero.tsx
@@ -1,10 +1,11 @@
 import { motion } from "framer-motion";
-
+import { useContext } from "react";
 import { HeroButton } from "../atoms/HeroButton";
-
+import AppContext from "../context/AppContext";
 import { useScrollToSection } from "../util/hooks";
 import { HeroText } from "../molecules/HeroText";
 import { Link } from "react-router-dom";
+import { SingleTreeLoader } from "atoms/interface/SingleTreeLoader";
 
 type HeroProps = {
   heroTextArray: string[];
@@ -19,25 +20,46 @@ export const Hero = ({
 }: HeroProps) => {
   const scrollToAbout = useScrollToSection("#about");
 
+  const { fontLoaded } = useContext(AppContext);
+
   return (
-    <motion.section
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: { duration: 0.3, easings: "ease-in-out" },
-      }}
-      className="relative w-screen h-screen carousel-item pt-16 bg-hero flex flex-col items-center justify-center"
-      id="hero"
-    >
-      <HeroText heroTextArray={heroTextArray} moving={moving} />
-      <div className="flex flex-col md:flex-row w-full  md:w-4/6 items-center justify-center space-x-0 space-y-5 md:space-x-5 md:space-y-0 relative bottom-[20%]">
-        <HeroButton onClick={scrollToAbout} size="xl">
-          Learn More
-        </HeroButton>
-        <HeroButton size="xl">
-          <Link to="/app">Explore the App</Link>
-        </HeroButton>
-      </div>
-    </motion.section>
+    <>
+      {!fontLoaded ? (
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { duration: 0.5, easings: "ease-in-out" },
+          }}
+          className="relative w-screen h-screen carousel-item pt-16 bg-hero flex flex-col items-center justify-center"
+          id="hero"
+        >
+          <SingleTreeLoader
+            treeNumber={0}
+            className="stroke-white w-1/2 h-1/2 relative bottom-8"
+          />
+        </motion.section>
+      ) : (
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { duration: 0.5, easings: "ease-in-out" },
+          }}
+          className="relative w-screen h-screen carousel-item pt-16 bg-hero flex flex-col items-center justify-center"
+          id="hero"
+        >
+          <HeroText heroTextArray={heroTextArray} moving={moving} />
+          <div className="flex flex-col md:flex-row w-full  md:w-4/6 items-center justify-center space-x-0 space-y-5 md:space-x-5 md:space-y-0 relative bottom-[20%]">
+            <HeroButton onClick={scrollToAbout} size="xl">
+              Learn More
+            </HeroButton>
+            <HeroButton size="xl">
+              <Link to="/app">Explore the App</Link>
+            </HeroButton>
+          </div>
+        </motion.section>
+      )}
+    </>
   );
 };
