ef44f01a1cb8a3f5474dbaf199b0c439b8726565
tverderesi <tverderesi@gmail.com>
2023-05-27 02:52:09 -0300

    update loader

--- client/src/components/layout/Loading.tsx
+++ client/src/components/layout/Loading.tsx
@@ -1,15 +1,20 @@
 /** @format */
 
-import TreeIcon from './icons/TreeIcon';
+import { SingleTreeLoader } from "atoms/interface/SingleTreeLoader";
+import TreeIcon from "./icons/TreeIcon";
 
 export default function Loading({ text, className }) {
   return (
     <div className={className}>
-      <div className='flex mx-auto'>
-        <TreeIcon treeNumber={0} /> <TreeIcon treeNumber={0} />
-        <TreeIcon treeNumber={0} />
+      <div className="flex mx-auto">
+        <SingleTreeLoader
+          treeNumber={0}
+          className="stroke-black h-full mx-auto"
+        />
       </div>
-      <div className='text-2xl mt-3 text-center font-semibold text-[var(--font-color)]'>{text}</div>
+      <div className="text-2xl mt-3 text-center font-semibold text-[var(--font-color)]">
+        {text}
+      </div>
     </div>
   );
 }
