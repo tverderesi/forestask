1aa163d42d0ac078618f0d6d93ee47a671daaafa
tverderesi <tverderesi@gmail.com>
2023-06-01 18:35:50 -0300

    refactor: animation

--- client/src/components/RegisterForm/SingleAvatar.tsx
+++ client/src/components/RegisterForm/SingleAvatar.tsx
@@ -1,6 +1,8 @@
 import { useContext, useState, useRef } from "react";
 import { SingleAvatarProps } from "../../types/Types";
 import RegisterContext from "../../context/RegisterContext";
+import { SingleTreeLoader } from "../../atoms/interface/SingleTreeLoader";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const SingleAvatar: React.FC<SingleAvatarProps> = ({ item }) => {
   const { profilePicture, dispatch } = useContext(RegisterContext);
@@ -26,19 +28,32 @@ export const SingleAvatar: React.FC<SingleAvatarProps> = ({ item }) => {
       className={`flex flex-col items-center pt-4 mt-2 w-24 floating-pic h-full carousel-item`}
       onClick={handleImageClick}
     >
-      {isLoading && (
-        <div className="animate-pulse w-14 h-14 rounded-full bg-gray-300 absolute"></div>
-      )}
-      <img
-        key={item}
-        src={`/media/avatars/${item}.jpg`}
-        className={`rounded-full w-14 h-14 ${
-          profilePicture === item ? "selected-avatar" : ""
-        }`}
-        alt={!isLoading ? item : ""}
-        onLoad={handleLoad}
-        onError={handleError}
-      />
+      <AnimatePresence>
+        {isLoading && (
+          <motion.div
+            key="loader"
+            className="w-14 h-14 rounded-full absolute"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <SingleTreeLoader className="stroke-black" />
+          </motion.div>
+        )}
+        <motion.img
+          key={item}
+          src={`/media/avatars/${item}.jpg`}
+          className={`rounded-full w-14 h-14 ${
+            profilePicture === item ? "selected-avatar" : ""
+          }`}
+          alt={!isLoading ? item : ""}
+          onLoad={handleLoad}
+          onError={handleError}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        />
+      </AnimatePresence>
 
       <span
         className={`capitalize ${
