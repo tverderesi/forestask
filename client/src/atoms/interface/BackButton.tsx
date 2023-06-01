26ba932258618d8da292fe69678610150b4a1301
tverderesi <tverderesi@gmail.com>
2023-06-01 18:37:29 -0300

    refactor: classname

--- client/src/atoms/interface/BackButton.tsx
+++ client/src/atoms/interface/BackButton.tsx
@@ -6,7 +6,7 @@ const BackButton: React.FC<BackButtonProps> = ({ href }) => {
   return (
     <Link
       to={href}
-      className="btn btn-sm btn-ghost bg-fandango-400 hover:bg-fandango-400 hover:lavender-web-50  active:bg-fandango-400 active:lavender-web-50  rounded-lg flex items-center justify-center text-lavender-web-50 mt-3"
+      className="btn btn-sm btn-ghost bg-fandango-400 hover:bg-fandango-400 hover:lavender-web-50  active:bg-fandango-400 active:lavender-web-50  rounded-lg flex items-center justify-center text-lavender-web-50 mt-3 w-auto"
     >
       <FaArrowLeft className="mr-2" />
       <FaHome className="text-xl" />
