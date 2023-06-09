4763da2763f4c6857de766b0d26fbd37ff618b89
tverderesi <tverderesi@gmail.com>
2023-06-08 21:15:34 -0300

    removed old hooks

--- client/src/util/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useForm } from "../hooks/useForm";
-
-import { useRotatingElements } from "../hooks/useRotatingElements";
-import { useScrollToSection } from "../hooks/useScrollToSection";
-
-export { useForm, useScrollToSection, useRotatingElements };
