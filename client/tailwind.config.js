9b00b3e9aabbab3227d8ffeb119943c49b23c9b4
tverderesi <tverderesi@gmail.com>
2023-06-08 21:09:50 -0300

    add tailwind formatter

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -251,7 +251,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require('prettier-plugin-tailwindcss')],
   daisyui: {
     themes: ["light", "dark"],
   },
