73e5bc5962fb962a0356450fb6879306e3b53215
Thomas Verderesi <tverderesi@gmail.com>
2024-03-25 22:08:01 -0300

    server: remove old files

--- apps/server/models/Subject.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-const subjectSchema = new Schema({
-  name: String,
-  teacher: String,
-  students: [{ student: String }],
-});
-
-module.exports = model('Subject', subjectSchema);
