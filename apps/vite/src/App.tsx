2bc7328b30435145544fd7784a9230c711832326
Thomas Verderesi <tverderesi@gmail.com>
2024-04-04 17:31:10 -0300

    update icon

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -14,27 +14,27 @@ export default App;
 export const LOL = () => (
   <>
     <section className="slide-section">
-      <TreeIcon className="h-96" />
+      <TreeIcon className="w-96  max-w-[90vw]" />
       <h1 className="font-semibold text-5xl ">森林の仕事 Forestask</h1>
     </section>
     <section className="slide-section">
-      <TreeIcon className="h-96" />
+      <TreeIcon className="w-96  max-w-[90vw]" />
       <h1 className="font-semibold text-5xl ">森林の仕事 Forestask</h1>
     </section>
     <section className="slide-section">
-      <TreeIcon className="h-96" />
+      <TreeIcon className="w-96  max-w-[90vw]" />
       <h1 className="font-semibold text-5xl ">森林の仕事 Forestask</h1>
     </section>{" "}
     <section className="slide-section">
-      <TreeIcon className="h-96" />
+      <TreeIcon className="w-96  max-w-[90vw]" />
       <h1 className="font-semibold text-5xl ">森林の仕事 Forestask</h1>
     </section>{" "}
     <section className="slide-section">
-      <TreeIcon className="h-96" />
+      <TreeIcon className="w-96  max-w-[90vw]" />
       <h1 className="font-semibold text-5xl ">森林の仕事 Forestask</h1>
     </section>{" "}
     <section className="slide-section">
-      <TreeIcon className="h-96" />
+      <TreeIcon className="w-96  max-w-[90vw]" />
       <h1 className="font-semibold text-5xl ">森林の仕事 Forestask</h1>
     </section>
   </>
